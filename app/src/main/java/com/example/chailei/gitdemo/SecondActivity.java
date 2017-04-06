8aa9d5bac0265ff28b9cff314a0f2c3ce83803ea
chailei <chailei9005@163.com>
2017-04-06 17:31:46 +0800

    add second activity

--- app/src/main/java/com/example/chailei/gitdemo/SecondActivity.java
+++ app/src/main/java/com/example/chailei/gitdemo/SecondActivity.java
@@ -2,14 +2,12 @@ package com.example.chailei.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("MainActivity","modify");
+        setContentView(R.layout.activity_second);
     }
 }
